48f2acd7157871ee045d8d98c963c30ff80a9d37
faruk öz <o.faruk.oz@hotmail.com>
2024-01-31 22:11:40 +0300

    test

--- WebApplication1/Pages/Error.cshtml.cs
+++ WebApplication1/Pages/Error.cshtml.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Pages
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
+        //testt
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
